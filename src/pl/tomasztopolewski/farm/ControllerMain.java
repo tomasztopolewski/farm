39f5bd57003e881ceee90fbd7b26281d7120cbb3
Tomasz Topolewski <tomasztopolewski21@gmai.com>
2018-05-09 20:52:23 +0200

    v0.00.115.1

--- src/pl/tomasztopolewski/farm/ControllerMain.java
+++ src/pl/tomasztopolewski/farm/ControllerMain.java
@@ -3,23 +3,14 @@ package pl.tomasztopolewski.farm;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static pl.tomasztopolewski.farm.Main.*;
-
 public class ControllerMain implements Initializable {
-    @FXML
-    Label labelTitle;
-
-    @FXML
-    Label labelFooter;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        labelTitle.setText(NAME);
-        labelFooter.setText(NAME + " v" + VERSION + " " + TYPE_VERSION + " by " + AUTHOR);
+
     }
 
 }
--- src/pl/tomasztopolewski/farm/Main.java
+++ src/pl/tomasztopolewski/farm/Main.java
@@ -1,31 +1,93 @@
 package pl.tomasztopolewski.farm;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import sun.rmi.runtime.Log;
 //import pl.tomasztopolewski.farm.app.ControllerMain;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Main extends Application {
+public class Main extends Application implements Initializable {
     public static final String NAME = "Farm";
     public static final String AUTHOR = "Tomasz Topolewski";
-    public static final String VERSION = "0.00.100";
-    public static final String TYPE_VERSION = "PRE-DEV";
+    public static final String VERSION = "0.00.115.1";
+    public static final String TYPE_VERSION = "pre-DEV";
 
-    public final static double HEIGHT_MAIN = 400.0, WIDTH_MAIN = 600.0;
+    public final static double  WIDTH_MAIN = 1000.0, HEIGHT_MAIN = 700.0;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    protected Scene sceneWelcome;
+    @FXML
+    protected Pane paneWelcome;
+    @FXML
+    protected Label labelTitle;
+    @FXML
+    protected Label labelFooter;
+
+
+    public static void main(String[] args) { launch(args); }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.setTitle(NAME);
-        primaryStage.setScene(new Scene(root, WIDTH_MAIN, HEIGHT_MAIN));
+        createLayoutWelcome();
+
+        //Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle(NAME + " v" + VERSION + " " + TYPE_VERSION);
+        sceneWelcome = new Scene(paneWelcome, WIDTH_MAIN, HEIGHT_MAIN);
+        //createLayoutWelcome();
+        primaryStage.setScene(sceneWelcome);
+
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
         primaryStage.show();
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        labelTitle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Kliknęto guzik.");
+            }
+        });
+    }
+
+
+
+    private void createLayoutWelcome() {
+        paneWelcome = new Pane();
+        paneWelcome.setPrefSize(WIDTH_MAIN, HEIGHT_MAIN);
+
+        labelTitle = new Label(new String(NAME).toUpperCase());
+        labelTitle.setPrefSize(1000.0, 140.0);
+        labelTitle.setLayoutX(0); labelTitle.setLayoutY(190.0);
+        labelTitle.setFont(new Font(150));
+        labelTitle.setAlignment(Pos.CENTER);
+
+        labelFooter = new Label(NAME + " v" + VERSION + " " + TYPE_VERSION + " by " + AUTHOR);
+        labelFooter.setPrefSize(1000.0, 60.0);
+        labelFooter.setLayoutX(0); labelFooter.setLayoutY(600.0);
+        labelFooter.setFont(new Font(16));
+        labelFooter.setAlignment(Pos.CENTER);
+
+        //sceneWelcome.setRoot(paneWelcome);
+        paneWelcome.getChildren().addAll(labelTitle, labelFooter);
+        //sceneWelcome.setRoot(paneWelcome);
+    }
+
+
 }
