9b5bacb24f8f1394dae165d96ad2f13626199e2e
Tomasz Topolewski <tomasztopolewski21@gmai.com>
2018-05-11 13:03:10 +0200

    v0.00.730.0

--- src/pl/tomasztopolewski/farm/Main.java
+++ src/pl/tomasztopolewski/farm/Main.java
@@ -2,8 +2,13 @@ package pl.tomasztopolewski.farm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import pl.tomasztopolewski.farm.preparation.Installation;
 import pl.tomasztopolewski.farm.preparation.Player;
@@ -13,7 +18,7 @@ import java.io.IOException;
 public class Main extends Application {
     public static final String NAME = "Farm";
     public static final String AUTHOR = "Tomasz Topolewski";
-    public static final String VERSION = "0.00.720.0";
+    public static final String VERSION = "0.00.730.0";
     public static final String TYPE_VERSION = "pre-DEV";
 
     public final static double WIDTH_MAIN = 1000.0, HEIGHT_MAIN = 700.0;
@@ -25,12 +30,6 @@ public class Main extends Application {
     Settings settings;
     Player player;
 
-    public static void main(String[] args) throws IOException {
-        launch(args);
-        new Logs().appendWithVersion("Aplikacja została wyłączona.");
-        new Logs(Logs.SYSTEM_WARN + "Nastąpiło wyłączenie aplikacji.");
-        new Logs().appendEmptyLine();
-    }
 
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
@@ -40,25 +39,76 @@ public class Main extends Application {
 
             rootWelcome = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
             setScene(new Scene(rootWelcome, WIDTH_MAIN, HEIGHT_MAIN));
+            createLayoutWelcome();
+            setScene(sceneWelcome);
+
+            stage.setTitle(NAME + " v" + VERSION + " " + TYPE_VERSION);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.show();
 
             new Logs().appendWithVersion("Wczytuję ustawienia.");
+            labelLoadingText.setText("Wczytuję ustawienia...");
             settings = new Settings();
+
             new Logs().appendWithVersion("Ładuję ustawienia i pliki gracza.");
+            labelLoadingText.setText("Ładuję ustawienia i pliki gracza...");
             player = new Player(settings.getValuesFromSettings());
 
         } else {
             rootCriticalError = FXMLLoader.load(getClass().getResource("CriticalError.fxml"));
             setScene(new Scene(rootCriticalError, WIDTH_MAIN, HEIGHT_MAIN));
-        }
 
-        stage.setTitle(NAME + " v" + VERSION + " " + TYPE_VERSION);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.sizeToScene();
-        stage.show();
+            stage.setTitle(NAME + " v" + VERSION + " " + TYPE_VERSION);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.show();
+        }
     }
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+        new Logs().appendWithVersion("Aplikacja została wyłączona.");
+        new Logs(Logs.SYSTEM_WARN + "Nastąpiło wyłączenie aplikacji.");
+        new Logs().appendEmptyLine();
+    }
+
 
     public void setScene(Scene scene) {
         this.scene = scene;
+    }
+
+
+    private Scene sceneWelcome;
+    private Pane paneWelcome;
+    private Label labelTitle, labelLoadingText, labelFooter;
+
+    private void createLayoutWelcome() {
+        paneWelcome = new Pane();
+        paneWelcome.setPrefSize(WIDTH_MAIN, HEIGHT_MAIN);
+
+        labelTitle = new Label(new String(NAME).toUpperCase());
+        labelTitle.setPrefSize(1000.0, 140.0);
+        labelTitle.setLayoutX(0); labelTitle.setLayoutY(190.0);
+        labelTitle.setFont(new Font("System Bold", 150));
+        labelTitle.setAlignment(Pos.CENTER);
+
+        labelLoadingText = new Label("Ładowanie aplikacji...");
+        labelLoadingText.setPrefSize(1000.0, 60.0);
+        labelLoadingText.setLayoutX(0); labelLoadingText.setLayoutY(420.0);
+        labelLoadingText.setFont(new Font("System Italic", 16));
+        labelLoadingText.setAlignment(Pos.CENTER);
+
+        labelFooter = new Label(NAME + " v" + VERSION + " " + TYPE_VERSION + " by " + AUTHOR);
+        labelFooter.setPrefSize(1000.0, 60.0);
+        labelFooter.setLayoutX(0); labelFooter.setLayoutY(600.0);
+        labelFooter.setFont(new Font(12)); labelFooter.setTextFill(Color.rgb(163, 163, 163));
+        labelFooter.setAlignment(Pos.CENTER);
+
+        paneWelcome.getChildren().addAll(labelTitle, labelLoadingText, labelFooter);
+
+        sceneWelcome = new Scene(paneWelcome, WIDTH_MAIN, HEIGHT_MAIN);
     }
 }
--- src/pl/tomasztopolewski/farm/core/Player.java
+++ src/pl/tomasztopolewski/farm/core/Player.java
@@ -7,7 +7,41 @@ public class Player {
     private double[] saveLoaded;
 
     public Player(String username, String id, String[] saveDownloaded) {
+        setUsername(username);
+        setId(id);
+        setSaveDownloaded(saveDownloaded);
 
     }
     public Player() {}
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getSaveDownloaded() {
+        return saveDownloaded;
+    }
+    public void setSaveDownloaded(String[] saveDownloaded) {
+        this.saveDownloaded = saveDownloaded;
+    }
+
+    public double[] getSaveLoaded() {
+        return saveLoaded;
+    }
+    public void setSaveLoaded(double[] saveLoaded) {
+        this.saveLoaded = saveLoaded;
+    }
+
+
+
 }
